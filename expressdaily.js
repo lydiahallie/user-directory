323a4888b934787fc2d6a2690ec59010f87fa114
lydiahallie <lydiajuliettehallie@gmail.com>
2017-07-25 21:10:22 -0400

    Changed layout index page

--- expressdaily.js
+++ expressdaily.js
@@ -1,13 +1,7 @@
 const express = require('express')
 const mustacheExpress = require('mustache-express')
 const data = require('./users.js')
-
 const app = express()
-
-app.listen(3000, () => {
-  console.log('Listening on port 3000')
-})
-
 app.use(express.static('public'))
 
 app.engine('mustache', mustacheExpress())
@@ -22,4 +16,8 @@ app.use(express.static('public'))
 app.get('/users/:username', (request, response) => {
   const username = request.params.username;
   response.render('user', username)
+})
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
 })
