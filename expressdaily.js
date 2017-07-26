3a1a83587a2bcca1fd5732c5efc19c62fec21412
lydiahallie <lydiajuliettehallie@gmail.com>
2017-07-26 09:28:16 -0400

    It works

--- expressdaily.js
+++ expressdaily.js
@@ -8,14 +8,22 @@ app.engine('mustache', mustacheExpress())
 app.set('views', './templates')
 app.set('view engine', 'mustache')
 
+app.use(express.static('public'))
+
 app.get('/', (request, response) => {
   response.render('index', data)
 })
 
-app.use(express.static('public'))
-app.get('/users/:username', (request, response) => {
+app.get('/user/:username', (request, response) => {
   const username = request.params.username;
-  response.render('user', username)
+  let user;
+  for (let i = 0; i < data.users.length; i++) {
+    if (data.users[i].username === username) {
+      user = data.users[i];
+      break;
+    }
+  }
+  response.render('user', user)
 })
 
 app.listen(3000, () => {
